3c9004b404fd027c91110e1b60c954c163acd000
aboelmagd1 <mahmoudaboelmagdfree@gmail.com>
2026-09-19 23:01:36 +0300

    update

--- UI/DimensionToggleButton.cs
+++ UI/DimensionToggleButton.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using ArcGIS.Desktop.Framework.Contracts;
 
 namespace GeoMetrics.UI
@@ -5,12 +8,20 @@ namespace GeoMetrics.UI
     /// <summary>
     /// Toggle button on the ribbon for GeoMetrics monitoring.
     /// Synchronizes bidirectionally with DimensionSettings.IsEnabled and provides
-    /// direct ON / OFF control from the top ribbon bar.
+    /// direct ON / OFF control from the top ribbon bar with dynamic icon switching.
     /// </summary>
     internal class DimensionToggleButton : Button
     {
+        private static ImageSource _onLargeImage;
+        private static ImageSource _onSmallImage;
+        private static ImageSource _offLargeImage;
+        private static ImageSource _offSmallImage;
+        private static bool _imagesLoaded;
+
         public DimensionToggleButton()
         {
+            EnsureImagesLoaded();
+
             var settings = Module1.Current?.Settings;
             if (settings != null)
             {
@@ -32,6 +43,23 @@ namespace GeoMetrics.UI
                 IsChecked = false;
                 UpdateAppearance(false);
             }
+        }
+
+        private static void EnsureImagesLoaded()
+        {
+            if (_imagesLoaded) return;
+            try
+            {
+                _onLargeImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsToggle_ON_32.png", UriKind.Absolute));
+                _onSmallImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsToggle_ON_16.png", UriKind.Absolute));
+                _offLargeImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsToggle_OFF_32.png", UriKind.Absolute));
+                _offSmallImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsToggle_OFF_16.png", UriKind.Absolute));
+            }
+            catch
+            {
+                // Graceful fallback to default DAML icons if pack URIs are unavailable
+            }
+            _imagesLoaded = true;
         }
 
         protected override void OnClick()
@@ -73,6 +101,17 @@ namespace GeoMetrics.UI
             Tooltip = isEnabled
                 ? "GeoMetrics is ON. Measurements update live during editing and selection. Click to turn OFF."
                 : "GeoMetrics is OFF. Click to turn ON real-time measurements.";
+
+            if (isEnabled)
+            {
+                if (_onLargeImage != null) LargeImage = _onLargeImage;
+                if (_onSmallImage != null) SmallImage = _onSmallImage;
+            }
+            else
+            {
+                if (_offLargeImage != null) LargeImage = _offLargeImage;
+                if (_offSmallImage != null) SmallImage = _offSmallImage;
+            }
         }
     }
 }
--- UI/ShowSettingsButton.cs
+++ UI/ShowSettingsButton.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Windows.Media.Imaging;
 using ArcGIS.Desktop.Framework.Contracts;
 
 namespace GeoMetrics.UI
@@ -5,6 +7,19 @@ namespace GeoMetrics.UI
     /// <summary>Button in the ribbon that opens the Settings Dock Pane.</summary>
     internal class ShowSettingsButton : Button
     {
+        public ShowSettingsButton()
+        {
+            try
+            {
+                LargeImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsSettings_32.png", UriKind.Absolute));
+                SmallImage = new BitmapImage(new Uri("pack://application:,,,/GeoMetrics;component/Images/GeoMetricsSettings_16.png", UriKind.Absolute));
+            }
+            catch
+            {
+                // Fallback to Config.daml declaration if pack URI cannot be resolved
+            }
+        }
+
         protected override void OnClick() => DimensionSettingsPaneViewModel.Show();
     }
 }
